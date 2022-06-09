cfbce93f7283bbe66fbef81966c533c801780e77
stefanGaina <stefan.gaina01@gmail.com>
2022-06-09 12:13:18 +0300

    Added window

---
+++ src/Window/Window.cpp
@@ -0,0 +1,62 @@
+#include "pch.h"
+#include "Window.h"
+
+Window::Window(const char* title, Uint32 position_x, Uint32 position_y, Uint16 screen_width, Uint16 screen_height, bool is_fullscreen)
+	: window_(nullptr), renderer_(nullptr)
+{
+	Sint32 success = 0;
+	Uint32 flag = is_fullscreen ? SDL_WINDOW_FULLSCREEN : 0;
+
+	TRACE("Window constructor.");
+
+	VERIFY(title != nullptr, "Window: Title is null!");
+	VERIFY(screen_width > 0, "Window: Screen width is 0!");
+	VERIFY(screen_height > 0, "Window: Screen height is 0!");
+	VERIFY(position_x == SDL_WINDOWPOS_CENTERED, "Window: Position is not centered! ({0})", position_x);
+	VERIFY(position_y == SDL_WINDOWPOS_CENTERED, "Window: Position is not centered! ({0})", position_y);
+
+	window_ = SDL_CreateWindow(title, position_x, position_y, screen_width, screen_height, flag);
+	ASSERT(window_ != nullptr, SDL_GetError());
+	INFO("Window successfully created!");
+
+	renderer_ = SDL_CreateRenderer(window_, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+	ASSERT(renderer_ != nullptr, SDL_GetError());
+	INFO("Renderer successfully created!");
+
+	SDL_SetRenderDrawBlendMode(renderer_, SDL_BLENDMODE_BLEND);
+	SDL_SetRenderDrawColor(renderer_, 0xFF, 0xFF, 0xFF, 0xFF); /* white */
+
+	//SDL_ShowCursor(SDL_DISABLE);
+
+	success = SDL_Init(SDL_INIT_EVERYTHING);
+	ASSERT(success >= 0, SDL_GetError());
+	INFO("SDL successfully initialized!");
+
+	/*if (TTF_Init() == -1)
+	{
+		ERROR(SDL_GetError());
+	}
+	if (Mix_OpenAudio(44100, MIX_DEFAULT_FORMAT, 2, 2048) < 0)
+	{
+		ERROR(SDL_GetError());
+	}*/
+}
+
+Window::~Window(void)
+{
+	SDL_DestroyWindow(window_);
+	window_ = nullptr;
+
+	SDL_DestroyRenderer(renderer_);
+	renderer_ = nullptr;
+
+	//TTF_Quit();
+	//IMG_Quit();
+	//Mix_Quit();
+	SDL_Quit();
+}
+
+SDL_Renderer* Window::getRenderer(void)
+{
+	return renderer_;
+}
--- src/Window/Window.h
+++ src/Window/Window.h
@@ -1,9 +1,19 @@
-#pragma once
+#ifndef WINDOW_H_
+#define WINDOW_H_
 
 #include <SDL.h>
 
 class Window
 {
+public:
+	Window(const char* title, Uint32 position_x, Uint32 position_y, Uint16 screen_width, Uint16 screen_height, bool is_fullscreen);
+	~Window(void);
+
+	SDL_Renderer* getRenderer(void);
+
 private:
 	SDL_Window* window_;
+	SDL_Renderer* renderer_;
 };
+
+#endif /* WINDOW_H_ */
